8ba2aaa113a1b28bf5c88f7a8c124b4ae0925cba
uruly <rk.uru007@gmail.com>
2021-05-23 23:40:50 +0900

    :rocket: Add CreateProjectSampleVC and Remove Main.storyboard #1

--- SwiftRecipesSample/Scripts/Views/CreateProjectSample/CreateProjectSampleViewController.swift
+++ SwiftRecipesSample/Scripts/Views/CreateProjectSample/CreateProjectSampleViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CreateProjectSampleViewController.swift
 //  SwiftRecipesSample
 //
 //  Created by Reo on 2021/05/23.
@@ -7,13 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class CreateProjectSampleViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
