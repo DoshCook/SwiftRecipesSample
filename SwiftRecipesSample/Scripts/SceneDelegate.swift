64d87bf8d6865ee42e96246939794af903df3999
uruly <rk.uru007@gmail.com>
2021-09-12 04:08:31 +0900

    :rocket: Add BasicLabelViewController.

--- SwiftRecipesSample/Scripts/SceneDelegate.swift
+++ SwiftRecipesSample/Scripts/SceneDelegate.swift
@@ -14,7 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let viewController = CreateProjectSampleViewController(nibName: "CreateProjectSampleViewController", bundle: nil)
+        let viewController = BasicLabelViewController(nib: R.nib.basicLabelViewController)
         window.rootViewController = viewController
         window.makeKeyAndVisible()
         self.window = window
---
+++ SwiftRecipesSample/Scripts/Views/BasicLabelSample/BasicLabelViewController.swift
@@ -0,0 +1,24 @@
+//
+//  BasicLabelViewController.swift
+//  SwiftRecipesSample
+//
+//  Created by Reo on 2021/09/11.
+//
+
+import UIKit
+
+final class BasicLabelViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+
+    @IBOutlet private var labels: [UILabel] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // ラベルの色をまとめて変更する
+        labels.forEach {
+            $0.textColor = .systemBlue
+        }
+    }
+}
